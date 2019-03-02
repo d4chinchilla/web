0b2f02d3eea7d5a7940324bdac63129e144efb98
Matt <mattcrossley99@gmail.com>
2019-03-02 15:20:53 +0000

    Modify install as microcontroller name keeps changing

--- install-daemon.sh
+++ install-daemon.sh
@@ -28,7 +28,7 @@ install()
 	    echo Unzipping!
         unzip /var/www/html/uploads/firmware.zip -d /var/www/html/fwExtract 2>file
         echo Extracting!
-        find /var/www/html/fwExtract -iname '*.bin' -exec cp {} /media/pi/NODE_L432KC1 \;
+        find /var/www/html/fwExtract -iname '*.bin' -exec cp {} /media/pi/NODE_L432KC*\;
     else
         echo Incorrectly signed file!
     fi
