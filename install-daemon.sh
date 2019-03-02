8c451618ea69e5b974be1c4ac190234740650242
Matt <mattcrossley99@gmail.com>
2019-03-02 19:10:28 +0000

    New keying

--- install-daemon.sh
+++ install-daemon.sh
@@ -23,6 +23,7 @@ install()
     echo INSTALLING!
     # Here, put code to verify an install file and install it
     lxterminal -e echo Hello!
+    rm /var/www/html/uploads/firmware.zip
     gpg -o /var/www/html/uploads/firmware.zip -d /var/www/html/uploads/firmware.zip.gpg
     result=$?
     echo $result
