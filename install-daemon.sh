93375b8fb0d078c601b949250d1917609910d98c
MattCrossley99 <mattcrossley99@gmail.com>
2019-03-04 10:22:49 +0000

    Whatever they did last night

--- install-daemon.sh
+++ install-daemon.sh
@@ -17,6 +17,12 @@ shutdown()
 start()
 {
     echo STARTING!
+
+    # MUY IMPORTANTE! This is a hack and must die soon. Goodnight.
+    cp /home/pi/D4/signal_processing/ARM/final_binary.bin /media/pi/NODE_L432KC
+    sleep 10
+    stty -F /dev/ttyACM0 406:0:18b4:8a30:3:1c:7f:15:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0
+    /var/www/backend &
     # Here, put code to start a new set of processes
 }
 
@@ -32,6 +38,7 @@ install()
     echo $result
     if [[ $result -eq "0" ]]; then
 	    echo Unzipping!
+	rm /var/www/html/fwExtract/*
         unzip /var/www/html/uploads/firmware.zip -d /var/www/html/fwExtract 2>file
         echo Extracting!
         find /var/www/html/fwExtract -iname '*.bin' -exec cp {} /media/pi/NODE_L432K* \;
@@ -48,6 +55,8 @@ reset()
 
 sleep 5
 install
+sleep 10
+start
 while true; do
     if read -r line < $fname; then
         echo $line
