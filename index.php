d7ef03c8923f13b1a07a8b02741cdcc4543b42a8
Matt <mattcrossley99@gmail.com>
2019-03-04 16:35:02 +0000

    Attempt to pull in json data

--- index.php
+++ index.php
@@ -34,10 +34,7 @@
                     data: [
                         {
                             type: "line",
-                            dataPoints: [
-                                {x: 1, y: 10},
-                                {x: 10, y: 1}
-                            ]
+                            dataPoints: FFTdata
                         }
                     ]
                 });
